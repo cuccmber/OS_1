3dd24736df25e6387b89ece933c975e37966cc9d
diane <yasuchenya83@gmail.com>
2020-11-19 17:13:01 +0300

    final commit

---
+++ cmake-build-debug/CMakeFiles/OS1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/diane/CLionProjects/OS1/main.c" "/home/diane/CLionProjects/OS1/cmake-build-debug/CMakeFiles/OS1.dir/main.c.o"
+  "/home/diane/CLionProjects/OS1/mmemory.c" "/home/diane/CLionProjects/OS1/cmake-build-debug/CMakeFiles/OS1.dir/mmemory.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
